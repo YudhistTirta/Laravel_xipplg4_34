afee4fadc573771e3a5147dc8f4f68c0e4dbf744
YuraYosikage <yudhayudhistira@gmail.com>
2025-11-04 08:48:30 +0700

    Add Feature Read Student

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LaandingController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\StudentController;
 
 Route::get('/', [LaandingController::class, 'index'])->name('landing');
 
-Route::prefix('admin')->group(function () {
+
+Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+
+    Route::resource('students', StudentController::class);
 });
